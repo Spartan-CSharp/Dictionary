5cb8b8033fca6edd638f2a513f8e076074688d16
Pierre Plourde <pierre@spartancsharp.net>
2024-02-28 11:28:53 -0500

    Create Code
    Create a Dictionary list of employee IDs and the name that goes with the ID. Fill in a few records. Then ask the user for their ID and return their name.

--- DictionaryConsoleApp/Program.cs
+++ DictionaryConsoleApp/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Dictionary<string, string> employees = new Dictionary<string, string>();
+
+bool done = false;
+
+do
+{
+	string? employeeId;
+	string? employeeName;
+
+	do
+	{
+		Console.Write("Enter Employee ID: ");
+		employeeId = Console.ReadLine();
+	} while ( string.IsNullOrWhiteSpace(employeeId) );
+
+	do
+	{
+		Console.Write("Enter Employee Name: ");
+		employeeName = Console.ReadLine();
+	} while ( string.IsNullOrWhiteSpace(employeeName) );
+
+	try
+	{
+		employees.Add(employeeId, employeeName);
+	}
+	catch ( ArgumentException ex )
+	{
+		Console.WriteLine(ex.Message);
+	}
+
+	Console.Write("Add another (yes/no): ");
+	string? addAnother = Console.ReadLine();
+
+	if ( addAnother?.ToLower() == "no" )
+	{
+		done = true;
+	}
+} while ( !done );
+
+Console.WriteLine();
+
+string? empId;
+
+do
+{
+	Console.Write("Enter your Employee ID: ");
+	empId = Console.ReadLine();
+} while ( string.IsNullOrWhiteSpace(empId) );
+
+try
+{
+	Console.WriteLine($"Welcome {employees[empId]}!");
+}
+catch ( KeyNotFoundException ex )
+{
+	Console.WriteLine(ex.Message);
+}
